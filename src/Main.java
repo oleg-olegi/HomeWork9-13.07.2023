038e7d67bc0db0541cc866713a8d8a95aa85eff1
User <ol.shink87@gmail.com>
2023-07-12 13:39:17 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static int[] generateRandomArray() {
@@ -15,11 +16,19 @@ public class Main {
     }
 
     public static void task1() {
+        System.out.println("Задача 1");
         int[] arr = generateRandomArray();
         int payouts = 0;
         for (int element : arr) {
             payouts += element;
         }
         System.out.println("Сумма трат за месяц составила " + payouts + " рублей");
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] arr = generateRandomArray();
+        Arrays.sort(arr);
+        System.out.println("Минимальная сумма трат за день составилa " + arr[0] + " рублей. Максимальная сумма трат за день составила " + arr[arr.length-1] + " рублей");
     }
 }
